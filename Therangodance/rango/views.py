a4338a8dc190dd10bfe902a9ae41d3fc2fab422f
Adam Berr <aberr5410@gmail.com>
2018-03-12 21:50:59 +0100

    The rythm is good, arrived at database and models page 40 now

--- Therangodance/rango/views.py
+++ Therangodance/rango/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Learning is never over ...')
+    context_dict = {'boldmessage' : 'Replacing the variable in the template engine'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'myname' : 'Med Larbi'}
+    return render(request, 'rango/about.html', context=context_dict)
